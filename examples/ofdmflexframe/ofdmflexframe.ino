dd896375d60012b4e6c9488bcb425e4191dcf922
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-12-16 09:27:25 +0100

    ofdm example

---
+++ examples/ofdmflexframe/ofdmflexframe.ino
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+#include <liquid.h>
+
+void setup() {
+    Serial.begin(115200);
+    // options
+    unsigned int M           = 64;          // number of subcarriers
+    unsigned int cp_len      = 16;          // cyclic prefix length
+    unsigned int taper_len   = 4;           // taper length
+    unsigned int payload_len = 120;         // length of payload (bytes)
+
+    // allocate memory for header, payload, sample buffer
+    unsigned int  buf_len = 200;            // buffer size
+    std::complex<float>  buf[buf_len];      // time-domain buffer
+    unsigned char header[8];                // header
+    unsigned char payload[payload_len];     // payload
+
+    // create frame generator object with default properties
+    ofdmflexframegen fg = ofdmflexframegen_create(M, cp_len, taper_len, NULL, NULL);
+
+    unsigned int i;
+
+    // initialize header/payload and assemble frame
+    for (i=0; i<8; i++)           header[i]  = i      & 0xff;
+    for (i=0; i<payload_len; i++) payload[i] = rand() & 0xff;
+    ofdmflexframegen_assemble(fg, header, payload, payload_len);
+    ofdmflexframegen_print(fg);
+
+    // generate frame one OFDM symbol at a time
+    int frame_complete = 0;
+    while (!frame_complete) {
+        // generate symbol (write samples to buffer)
+        frame_complete = ofdmflexframegen_write(fg, buf, buf_len);
+
+        // print status
+        printf("ofdmflexframegen wrote to buffer%s\n",
+            frame_complete ? " (frame complete)" : "");
+    }
+
+    // destroy objects and return
+    ofdmflexframegen_destroy(fg);
+    printf("done.\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
